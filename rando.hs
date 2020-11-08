51c73bd9c7e283afc85fe3821b3b43be86a62610
jamin <acdimalev@gmail.com>
2020-11-07 20:23:06 -0800

    annotate Rando module

--- rando.hs
+++ rando.hs
@@ -3,6 +3,33 @@ module Rando where
 import Data.Bits
 import Control.Monad.Trans.State
 
+-- an implementation of a (bad) Psuedo-Random Number Generator
+-- this PRNG was designed for Gameboy hardware
+
+-- a four-bit shift register with a cycle of 15
+
+rand_f x =
+  let b1 = (x `shiftR` 3) `xor` (x `shiftR` 2) .&. 1
+  in (x `shiftL` 1) .&. 15 .|. b1
+
+-- a relatively-prime walk of Z16
+
+rand_g x = (x + 9) .&. 15
+
+-- trivial combination of rand1 and rand2 for a cycle of 240
+-- excludes values in the range 0..15
+
+rand x =
+  let
+    a = rand_f (x `shiftR` 4) `shiftL` 4
+    b = rand_g (x .&. 15)
+  in a .|. b
+
+rand0 = 16
+
+-- filters for `rand` with "good" PRNG characteristics
+-- good for an eight-bit PRNG, anyway
+
 d2 x = 1 .&. ((x `shiftR` 4) `xor` (x `shiftR` 3))
 d3 x = x `mod` 3
 d4 x = 3 .&. ((x `shiftR` 4) `xor` (x `shiftR` 2))
@@ -10,17 +37,7 @@ d5 x = x `mod` 5
 d6 x = ((x `shiftR` 4) `xor` x) `mod` 6
 d8 x = 7 .&. ((x `shiftR` 4) `xor` (x `shiftR` 1))
 
-rand1 x =
-  let b1 = (x `shiftR` 3) `xor` (x `shiftR` 2) .&. 1
-  in (x `shiftL` 1) .&. 15 .|. b1
-
-rand2 x = (x + 9) .&. 15
-
-rand x =
-  let
-    a = rand1 (x `shiftR` 4) `shiftL` 4
-    b = rand2 (x .&. 15)
-  in a .|. b
+-- State Monad variants
 
 roll :: State Int Int
 roll = state $ \s -> (rand s, rand s)
@@ -31,6 +48,8 @@ rollD4 = fmap d4 roll
 rollD5 = fmap d5 roll
 rollD6 = fmap d6 roll
 rollD8 = fmap d8 roll
+
+-- scratch notes for reference
 
 -- q = [16, 41, 66, 155, 52, 109, 214, 175, 88, 177, 122, 243, 236, 197, 142, 23]
 -- [1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1] == fmap d2 q
